1ee5481418224d9dacaa1e3bb371dd7b5b26cac7
Sean <leoseanquilty@gmail.com>
2017-08-01 15:52:08 +0000

    Initializing project dependancy express, and editing package.json to include pertinent details

---
+++ express_server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+let urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.ca"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!\n");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req ,res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});

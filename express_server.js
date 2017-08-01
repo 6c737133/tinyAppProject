f9b5270059ae7084d80ff1120e573ed4e45615e8
Sean <leoseanquilty@gmail.com>
2017-08-01 15:56:42 +0000

    Defining the view engine as  EJS

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.set("view engine", "ejs";)
+
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.ca"

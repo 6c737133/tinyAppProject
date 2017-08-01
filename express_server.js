74f19fd8f55bfad0d1bbb7bde451b252b34e4528
Sean <leoseanquilty@gmail.com>
2017-08-01 18:10:17 +0000

    Created view template 'url_show.ejs' and added routing logic for it in 'express_server.js'

--- express_server.js
+++ express_server.js
@@ -26,6 +26,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id,
+  longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
